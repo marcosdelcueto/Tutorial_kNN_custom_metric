bd3a580312005b1f120a1859b0e8347455a7edef
Marcos del Cueto <marcos.delcueto91@gmail.com>
2020-08-20 11:59:21 +0100

    Update files

--- kNN_regression.py
+++ kNN_regression.py
@@ -1,90 +1,59 @@
 #!/usr/bin/env python3
 # Marcos del Cueto
+# Import necessary libraries
 import math
-from math import sqrt
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor, DistanceMetric
-from sklearn.model_selection import StratifiedKFold, train_test_split, learning_curve, LeaveOneOut, KFold
-from sklearn.metrics import mean_squared_error,make_scorer
-from scipy.stats import pearsonr, spearmanr
-
+from sklearn.model_selection import LeaveOneOut
+from sklearn.metrics import mean_squared_error
+from scipy.stats import pearsonr
 
 ### Function just used to test custom metrics ###
 def mimic_minkowski(X1,X2):
-    distance=0.0
-    #print('new call minkowski')
-    #print('X1:')
-    #print(X1)
-    #print('X2:')
-    #print(X2)
-    for i in range(len(X1)):
-        distance=distance+(X1[i]-X2[i])**2
-    distance=distance**(1.0/2.0)
-    #print('distance:', distance)
-    #print('##################')
+    distance=0.0                            # Initialize distance
+    for i in range(len(X1)):                # For each element in X1 (in this case it is just 1D)
+        distance=distance+(X1[i]-X2[i])**2  # Make sum of squared differences
+    distance=math.sqrt(distance)            # Calculate final distance as sqrt of previous sum
     return distance
-
 ### Function just used to test custom metrics ###
 def custom_metric(X1,X2):
-    #diff=0.0
-    #for i in range(len(X1)):
-        #diff=diff+(X1[i]-X2[i])**2
-    #diff=diff**(1.0/2.0)
-    #print('diff:', diff)
-    #print('##################')
-    diff = X1[0]-X2[0]
-    diff = 360/365*diff
-    diff = diff * math.pi/180
-    distance = sqrt(150e6*(1-math.cos(diff)))
+    diff = X1[0]-X2[0]                      # Calculate Day difference between X1 and X2
+    diff = 360/365*diff                     # Transforms Day difference to angle difference
+    diff = diff * math.pi/180               # Transform degree to radians
+    distance = math.sqrt(1-math.cos(diff))  # Calculate distance in polar coordinates
     return distance
-
-db_file='canada_average.csv'
-Neighbors=[1,2,3,4,5,6,7,8,9,10]
-kfold=50
-df=pd.read_csv(db_file,index_col=0)
-
-print (df)
-
-X = df['Day'].values
-y = df['T (deg C)'].values
-#print('X:')
-#print(X)
-#print('y:')
-#print(y)
-
-X.reshape(-1, 1)
-y.reshape(-1, 1)
-
-
-
-
-
+###################### MAIN CODE ######################
+# Read data
+db_file='dataset.csv'                       # Name of csv file with dataset
+df=pd.read_csv(db_file,index_col=0)         # Read dataset into a dataframe
+X = df['Day'].values                        # Assign 'Day' descriptor
+y = df['T (deg C)'].values                  # Assign 'T (deg C)' target property
+# kNN regression
+Neighbors=[1,2,3,4,5,6,7,8,9,10]            # Specify number of neighbors k used in grid search
+# Grid search loop
 for k in Neighbors:
-    print('NEW NEIGHBOR',k)
-    ML_algorithm = KNeighborsRegressor(n_neighbors=k, weights='distance')
-    #ML_algorithm = KNeighborsRegressor(n_neighbors=k, weights='distance', metric=mimic_minkowski)
-    #ML_algorithm = KNeighborsRegressor(n_neighbors=k, weights='distance', metric=custom_metric)
+    # Initialize lists
     y_predicted=[]
     y_real=[]
-    kf = KFold(n_splits=kfold,shuffle=True)
-    validation=kf.split(X)
-    loo = LeaveOneOut()
-    validation=loo.split(X)
-    for train_index, test_index in validation:
-        #print('NEW FOLD')
+    # Specify options for kNN regression
+    kNNreg = KNeighborsRegressor(n_neighbors=k, weights='distance', metric=custom_metric)
+    # Leave-one-out loop
+    for train_index, test_index in LeaveOneOut().split(X):
+        # Assign train/test values
         X_train,X_test=X[train_index],X[test_index]
         y_train,y_test=y[train_index],y[test_index]
         X_train = X_train.reshape(-1, 1)
         X_test = X_test.reshape(-1, 1)
-        y_pred = ML_algorithm.fit(X_train, y_train.ravel()).predict(X_test)
+        # Predict data
+        y_pred = kNNreg.fit(X_train, y_train.ravel()).predict(X_test)
+        # Append data of each leave-one-out iteration
         y_predicted.append(y_pred.tolist())
         y_real.append(y_test.tolist())
-    
+    # Flatten lists with real and predicted values
     y_real = [item for dummy in y_real for item in dummy ]
     y_predicted = [item for dummy in y_predicted for item in dummy ]
-    #print('y_predicted:', y_predicted)
-    #print('y_real:', y_real)
-    
+    # Calculate r and rmse metrics
     r, _ = pearsonr(y_real, y_predicted)
-    rms  = sqrt(mean_squared_error(y_real, y_predicted))
-    print('Neighbors:',k,'r:',r,'rmse:',rms)
+    rmse  = math.sqrt(mean_squared_error(y_real, y_predicted))
+    # Print results for each k value in the grid search
+    print('Neighbors: %i. r: %.3f. rmse: %.3f' %(k, r, rmse))
